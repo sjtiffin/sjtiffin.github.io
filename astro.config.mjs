e6041d3e4394c1068f5940e64620d04b8ab6f1d0
sara <sara@ocelot.local>
2024-11-25 12:59:38 -0600

    remove base from astro.config.mjs

--- astro.config.mjs
+++ astro.config.mjs
@@ -6,6 +6,5 @@ import tailwind from '@astrojs/tailwind';
 // https://astro.build/config
 export default defineConfig({
   site: 'https://sjtiffin.github.io',
-  base: 'sjtiffin.github.io',
   integrations: [tailwind()]
 });
