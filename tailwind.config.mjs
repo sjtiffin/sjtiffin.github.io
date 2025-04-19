a2419b9374ba52e8a19a07224c961da34f8a5d22
sara <sarajtiffin@gmail.com>
2025-04-19 13:06:23 -0500

    update to latest version of astro, tailwindcss, daisyui

--- tailwind.config.mjs
+++
@@ -1,8 +0,0 @@
-/** @type {import('tailwindcss').Config} */
-export default {
-	content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
-	plugins: [require("@tailwindcss/typography"), require("daisyui")],
-	daisyui: {
-		themes: ["night", "winter", "synthwave"],
-	}
-}
